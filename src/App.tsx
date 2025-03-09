9edab8e185e1b5e2206868c344f2684440660293
Ishaa Sampla <ixs289@student.bham.ac.uk>
2025-03-09 22:06:34 +0000

    removed fake path

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<div> about me</div>} />
 				</Routes>
 			</BrowserRouter>
 		</div>
