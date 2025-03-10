5fabac067b090a8a8726b7c309cf6f05541499ef
Ishaa Sampla <ixs289@student.bham.ac.uk>
2025-03-10 18:32:25 +0000

    created route for about me page and added taskbar

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import { Taskbar } from "../components/Sidebar";
+import "../style/About.css";
+
+export default function About() {
+	return (
+		<div className="about-me">
+			<Taskbar />
+		</div>
+	);
+}
