b708db7314d7978567eebfff9b8a511a516acb30
Ishaa Sampla <ixs289@student.bham.ac.uk>
2025-03-10 22:29:23 +0000

    added a box border and hid the overflow. moved css to root css file

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,8 +6,8 @@ export default function About() {
 	return (
 		<div className="page">
 			<Taskbar />
-			<Grid container spacing={2}>
-				<Grid size={{ xs: 12, sm: 7, md: 7 }} className="container">
+			<Grid container spacing={2} className="grid-container">
+				<Grid size={{ xs: 12, sm: 7, md: 7 }} className="grid">
 					<Introduction />
 				</Grid>
 				<Grid size={{ xs: 12, sm: 5, md: 5 }}></Grid>
